e04ea121d5e29eb8c606397fc7ca33d9b22c4923
AikyDJ <96826093+AikyDJ@users.noreply.github.com>
2026-02-09 17:00:01 +0300

    add view test

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,12 +1,11 @@
 <?php
 
-use app\controllers\ApiExampleController;
 use app\middlewares\SecurityHeadersMiddleware;
 use flight\Engine;
 use flight\net\Router;
-
-/** 
- * @var Router $router 
+use app\controllers\AdminController;
+/**
+ * @var Router $router
  * @var Engine $app
  */
 
@@ -17,14 +16,7 @@ $router->group('', function(Router $router) use ($app) {
 		$app->render('welcome', [ 'message' => 'You are gonna do great things!' ]);
 	});
 
-	$router->get('/hello-world/@name', function($name) {
-		echo '<h1>Hello world! Oh hey '.$name.'!</h1>';
-	});
+	$router->get('/admin', [AdminController::class, 'dashboard']);
 
-	$router->group('/api', function() use ($router) {
-		$router->get('/users', [ ApiExampleController::class, 'getUsers' ]);
-		$router->get('/users/@id:[0-9]', [ ApiExampleController::class, 'getUser' ]);
-		$router->post('/users/@id:[0-9]', [ ApiExampleController::class, 'updateUser' ]);
-	});
 	
 }, [ SecurityHeadersMiddleware::class ]);
--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -2,4 +2,23 @@
 
 <!-- list of users -->
 <!-- list of echange -->
+<table>
+    <th>id</th>
+    <th>obj-1-id</th>
+    <th>obj-1-id</th>
+    <th>user-1-id</th>
+    <th>user-2-id</th>
+    <th>status</th>
+    <?php foreach ($exchanges as $exchange) : ?>
+        <tr>
+            <td><?= $exchange['id'] ?></td>
+            <td><?= $exchange['object1'] ?></td>
+            <td><?= $exchange['object2'] ?></td>
+            <td><?= $exchange['user1'] ?></td>
+            <td><?= $exchange['user2'] ?></td>
+            <td><?= $exchange['status'] ?></td>
+        </tr>
+    <?php endforeach; ?>
+
+</table>
 <!-- total echange -->
