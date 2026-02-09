bcc14f31228c49de01041e31396f80608207d4c1
AikyDJ <96826093+AikyDJ@users.noreply.github.com>
2026-02-09 17:00:01 +0300

    add view test

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,17 +1,27 @@
 <?php
 namespace app\controllers;
 
+use app\service\AdminService;
+
 use flight\Engine;
 
 class AdminController {
 
     protected Engine $app;
 
+    protected AdminService $adminService;
     public function __construct($app) {
         $this->app = $app;
+        $this->adminService = new AdminService();
     }
 
     public function dashboard() {
-        $this->app->render('admin/dashboard');
+
+        $exchanges = $this->adminService->getexchages();
+
+        $this->app->render('admin/dashboard',[
+            'exchanges' => $exchanges
+        ]);
     }
+
 }
